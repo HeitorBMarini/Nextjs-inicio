47d3f2b81e1f142dacfaf1ff7fbee4a7bf2a84cd
HeitorBMarini <mariniheitor@gmail.com>
2023-05-19 20:02:27 -0300

    first commit

---
+++ pages/faq.js
@@ -0,0 +1,9 @@
+export default function FAQPage(){
+    return(
+
+        <div>
+            <h1> Teste</h1>
+            <a href="/"> Voltar pra home</a>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return(
+        <div> 
+            <h1> Home Page- Alura Cases hehe</h1>
+            <a href="/faq"> Link de caminho</a>
+        </div>
+    )
+}
