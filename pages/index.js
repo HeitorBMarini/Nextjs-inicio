1752781ec75cf4ce8c24f7cb88477007cf0d2d0a
HeitorBMarini <mariniheitor@gmail.com>
2023-05-19 20:39:17 -0300

    ccriando primeiro component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import Link from '../src/components/Link';
+
+
 export default function HomePage() {
-    return(
-        <div> 
-            <h1> Home Page- Alura Cases hehe</h1>
-            <a href="/faq"> Link de caminho</a>
+    return (
+        <div>
+            <h1>Alura Cases - Home Page</h1>
+            <Link href="/faq">
+                Ir para a página do FAQ
+            </Link>
         </div>
     )
 }
