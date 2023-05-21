04b71a085771bc9bcdad183a6c84dd337ff0fe80
HeitorBMarini <mariniheitor@gmail.com>
2023-05-21 09:53:15 -0300

    vendo mais de Props no Next

--- pages/404.js
+++ pages/404.js
@@ -3,10 +3,16 @@ import Link from '../src/components/Link';
 export default function Page404() {
     return (
         <div>
-            <h1>Você se perdeu e caiu na página 404 :O</h1>
-            <Link href="/">
+            <h1 style={{ color: 'red' }}>Você se perdeu e caiu na página 404, IT´S A TRAAAP :O</h1>
+            <Link href="/" className="link-home">
                 Ir para a home
             </Link>
+            <style jsx>{`
+                .link-home {
+                   margin-top: 10rem;
+                    
+                }
+            `}</style>
         </div>
-    )
+    );
 }
